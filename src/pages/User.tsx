7bb711d60468292f6ac73696eae40946ba318273
roboticx <249742614+roboticx@users.noreply.github.com>
2026-02-22 11:02:35 +0530

    Fix import statement formatting in User.tsx

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { FETCH, PATCH } from "../utils/apiUtils";
 interface UserType {
